426b56326d9bd5e5fca07bed2a20b0c1b1b74969
liuguangzhao <liuguangzhao@users.sf.net>
2011-12-16 17:29:37 +0800

    clock and razor core lib zh_CN translation

---
+++ libraries/razorqt/translations/librazorqt_zh_CN.ts
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>AddPluginDialog</name>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="14"/>
+        <source>Add plugins</source>
+        <translation type="unfinished">添加插件</translation>
+    </message>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="22"/>
+        <source>Search:</source>
+        <translation type="unfinished">搜索：</translation>
+    </message>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="52"/>
+        <source>Add plugin</source>
+        <translation type="unfinished">添加插件</translation>
+    </message>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="59"/>
+        <source>Close</source>
+        <translation type="unfinished">关闭</translation>
+    </message>
+</context>
+<context>
+    <name>Battery</name>
+    <message>
+        <location filename="../librazor-upower/battery.cpp" line="68"/>
+        <source>%1 : Properties changed</source>
+        <translation type="unfinished">%1 ： 属性已修改</translation>
+    </message>
+    <message>
+        <location filename="../librazor-upower/battery.cpp" line="74"/>
+        <source>%1 : Low</source>
+        <translation type="unfinished">% ： 低</translation>
+    </message>
+    <message>
+        <location filename="../librazor-upower/battery.cpp" line="84"/>
+        <source>%1 : Full</source>
+        <translation type="unfinished">%1 ： 满</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManager</name>
+    <message>
+        <location filename="../powermanager.cpp" line="94"/>
+        <source>Hibernate</source>
+        <translation type="unfinished">休眠</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="101"/>
+        <source>Suspend</source>
+        <translation type="unfinished">挂起</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="108"/>
+        <source>Reboot</source>
+        <translation type="unfinished">重启</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="115"/>
+        <source>Shutdown</source>
+        <translation type="unfinished">关机</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="120"/>
+        <source>Logout</source>
+        <translation type="unfinished">登出</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="132"/>
+        <source>Razor Session Suspend</source>
+        <translation type="unfinished">挂起 Razor 会话</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="133"/>
+        <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
+        <translation type="unfinished">是否挂起你的电脑？&lt;p&gt;电脑将挂起到低耗电状态。如果掉电，系统状态将丢失。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="144"/>
+        <source>Razor Session Hibernate</source>
+        <translation type="unfinished">Razor 会话休眠</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="145"/>
+        <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
+        <translation type="unfinished">是否休眠你的电脑？&lt;p&gt;电脑将休眠到低耗电状态。如果掉电，系统状态将丢失。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="156"/>
+        <source>Razor Session Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="157"/>
+        <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="168"/>
+        <source>Razor Session Shutdown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="169"/>
+        <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="180"/>
+        <source>Razor Session Logout</source>
+        <translation type="unfinished">登出 Razor 会话</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="181"/>
+        <source>Do you want to really logout? All unsaved work will be lost...</source>
+        <translation type="unfinished">真的要登出吗？没有保存的工作将会丢失。。。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="193"/>
+        <location filename="../powermanager.cpp" line="198"/>
+        <source>Razor Power Manager Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="193"/>
+        <source>Hibernate failed.</source>
+        <translation type="unfinished">休眠到磁盘失败。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="198"/>
+        <source>Suspend failed.</source>
+        <translation type="unfinished">扶起到内存失败。</translation>
+    </message>
+</context>
+<context>
+    <name>RazorShortcutButton</name>
+    <message>
+        <location filename="../razorshortcutbutton.cpp" line="48"/>
+        <source>Clear</source>
+        <translation type="unfinished">清理</translation>
+    </message>
+    <message>
+        <location filename="../razorshortcutbutton.cpp" line="160"/>
+        <source>None</source>
+        <translation type="unfinished">无</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenSaver</name>
+    <message>
+        <location filename="../screensaver.cpp" line="48"/>
+        <source>Lock Screen</source>
+        <translation type="unfinished">锁定屏幕</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="67"/>
+        <location filename="../screensaver.cpp" line="75"/>
+        <location filename="../screensaver.cpp" line="83"/>
+        <location filename="../screensaver.cpp" line="95"/>
+        <location filename="../screensaver.cpp" line="103"/>
+        <location filename="../screensaver.cpp" line="111"/>
+        <location filename="../screensaver.cpp" line="120"/>
+        <source>Screen Saver Activation Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="68"/>
+        <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="76"/>
+        <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="84"/>
+        <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="96"/>
+        <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="104"/>
+        <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="112"/>
+        <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="121"/>
+        <source>An error occurred starting screensaver. Unkown error - undocumented return value from xdg-screensaver=%1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../razoraboutdlg.ui" line="14"/>
+        <source> About Razor</source>
+        <translation type="unfinished">关于 Razor</translation>
+    </message>
+</context>
+</TS>
---
+++ razorqt-desktop/desktop-razor/plugin-analogclock/translations/analogclock_zh_CN.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>Clock</name>
+    <message>
+        <location filename="../clock.cpp" line="90"/>
+        <source>Clock</source>
+        <translation>时钟</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog.ui" line="14"/>
+        <source>Analog clock settings</source>
+        <translation>模拟时间设置</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog.ui" line="23"/>
+        <source>Show &amp;seconds hand</source>
+        <translation>显示秒针(&amp;S)</translation>
+    </message>
+</context>
+</TS>
