ec94414427734ccd522a050f5c83ed312aa5957a
liuguangzhao <liuguangzhao@users.sf.net>
2011-12-16 15:42:36 +0800

    add zh_CN translation for razor-panel

---
+++ razorqt-panel/panel/translations/razor-panel_zh_CN.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>PositionAction</name>
+    <message>
+        <location filename="../razorpanel.cpp" line="495"/>
+        <source>Top of desktop</source>
+        <translation type="unfinished">顶部</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="496"/>
+        <source>Bottom of desktop</source>
+        <translation type="unfinished">底部</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="497"/>
+        <source>Left of desktop</source>
+        <translation type="unfinished">左侧</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="498"/>
+        <source>Right of desktop</source>
+        <translation type="unfinished">右侧</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="505"/>
+        <source>Top of desktop %1</source>
+        <translation type="unfinished">%1 的顶部</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="506"/>
+        <source>Bottom of desktop %1</source>
+        <translation type="unfinished">%1 的底部</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="507"/>
+        <source>Left of desktop %1</source>
+        <translation type="unfinished">%1 的左侧</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="508"/>
+        <source>Right of desktop %1</source>
+        <translation type="unfinished">%1 的右侧</translation>
+    </message>
+</context>
+<context>
+    <name>RazorPanelPrivate</name>
+    <message>
+        <location filename="../razorpanel.cpp" line="689"/>
+        <source>Panel</source>
+        <translation type="unfinished">面板</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="700"/>
+        <source>Plugins</source>
+        <translation type="unfinished">插件</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="702"/>
+        <source>Add plugins ...</source>
+        <translation type="unfinished">添加插件...</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="711"/>
+        <source>Move plugin</source>
+        <translation type="unfinished">移动插件</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="717"/>
+        <source>Configure plugin</source>
+        <translation type="unfinished">配置插件</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="724"/>
+        <source>Delete plugin</source>
+        <translation type="unfinished">删除插件</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="731"/>
+        <source>Show this panel at</source>
+        <translation type="unfinished">显示面板于</translation>
+    </message>
+</context>
+</TS>
